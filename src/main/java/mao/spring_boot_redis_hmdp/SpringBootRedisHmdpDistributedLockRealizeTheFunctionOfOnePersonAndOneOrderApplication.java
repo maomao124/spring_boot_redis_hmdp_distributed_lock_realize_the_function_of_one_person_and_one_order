217ed898d76d4f5177ffea00e9badeb5280984a2
mao <1296193245@qq.com>
2022-05-17 10:21:59 +0800

    spring_boot_redis_hmdp_distributed_lock_realize_the_function_of_one_person_and_one_order

---
+++ src/main/java/mao/spring_boot_redis_hmdp/SpringBootRedisHmdpDistributedLockRealizeTheFunctionOfOnePersonAndOneOrderApplication.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_redis_hmdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisHmdpDistributedLockRealizeTheFunctionOfOnePersonAndOneOrderApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootRedisHmdpDistributedLockRealizeTheFunctionOfOnePersonAndOneOrderApplication.class, args);
+    }
+
+}
